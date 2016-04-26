bd39de9fdcc097fa76033482fd50af98f0056265
Ubuntu <ubuntu@ip-172-31-0-73.ec2.internal>
2016-04-26 21:39:14 +0000

    minor changes2

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
 
 	// the database url to connect!
-	url: 'mongodb://todoAholic:scotchio@ec2-52-0-14-185.compute-1.amazonaws.com:27017/dummyDB'
+	url: 'mongodb://ec2-54-88-127-104.compute-1.amazonaws.com:27017/dummyDB'
 }
