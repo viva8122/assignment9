668dda617997f78ee44f1b346058b8a44edc23fc
viva8122 <viva8122@gmail.com>
2016-05-19 11:30:54 -0700

    Make it better

--- app/routes.js
+++ app/routes.js
@@ -40,6 +40,7 @@ module.exports = function(app) {
 
 	// application -------------------------------------------------------------
 	app.get('*', function(req, res) {
-		res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+		res.sendfile('./client/index.html');
+		//res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
 	});
 };
