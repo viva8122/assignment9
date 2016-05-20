ffae74f05a8eb8a3a15cb1f64003ebf749c99161
viva8122 <viva8122@gmail.com>
2016-05-20 09:04:51 -0700

    Make it better

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
 
 	// the database url to connect!
-	//url: 'mongodb://ec2-54-88-127-104.compute-1.amazonaws.com:27017/dummyDB' // old
-	url: 'mongodb://ec2-52-37-1-24.us-west-2.compute.amazonaws.com:27017/dummyDB'	
+	url: 'mongodb://ec2-54-88-127-104.compute-1.amazonaws.com:27017/dummyDB1' // old
+	//url: 'mongodb://ec2-52-37-1-24.us-west-2.compute.amazonaws.com:27017/dummyDB01'	
 }
